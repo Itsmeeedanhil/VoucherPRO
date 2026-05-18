1581526379dc6690ced04240f27268d0835cc2b3
Itsmeeedanhil <djampatin@gmail.com>
2026-05-18 12:45:50 +0800

    MPMSICO NEW CLIENT

---
+++ VoucherPROVER2/Clients/MPM/Dashboard_MPM.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace VoucherPROVER2.Clients.MPM
+{
+    partial class Dashboard_MPM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Dashboard_MPM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Dashboard_MPM";
+            this.Text = "Dashboard_MPM";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
--- VoucherPROVER2/Dashboard.cs
+++ VoucherPROVER2/Dashboard.cs
@@ -8,12 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using VoucherPROVER2.Clients.IVP;
+using VoucherPROVER2.Clients.MPM;
 
 namespace VoucherPROVER2
 {
     public class GlobalVariables
     {
-        public static string client = "IVP";
+        public static string client = "MPM";
         public static bool includeImage = true;
         public static bool includeItemReceipt = true;
         public static bool testWithoutData = true;
@@ -51,6 +52,17 @@ namespace VoucherPROVER2
 
                 // 3. Add that panel into the current panel's controls
                 panel.Controls.Add(ivpContent);
+            }
+            if (GlobalVariables.client == "MPM")
+            {
+                // 1. Instantiate the specific dashboard class
+                Dashboard_MPM dashboard_MPM = new Dashboard_MPM();
+
+                // 2. Call the method that returns the panel
+                Panel mpmContent = dashboard_MPM.ContainerPanel();
+
+                // 3. Add that panel into the current panel's controls
+                panel.Controls.Add(mpmContent);
             }
             else
             {
